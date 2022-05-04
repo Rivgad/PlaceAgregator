86aa7939e7fba5c1228a486c9f1cb4a9015db5b7
Rivgad <losvermo@gmail.com>
2022-05-04 05:30:10 +0300

    account login unique

--- PlaceAgregator/PlaceAgregator.EntityFramework/ApplicationContext.cs
+++ PlaceAgregator/PlaceAgregator.EntityFramework/ApplicationContext.cs
@@ -62,6 +62,10 @@ namespace PlaceAgregator.EntityFramework
                 .HasForeignKey<User>(item => item.AccountId)
                 .OnDelete(DeleteBehavior.Cascade);
 
+            modelBuilder.Entity<Account>()
+                .HasIndex(item => item.Login)
+                .IsUnique();
+
             modelBuilder.Entity<BookingRequestServiceItem>(
                 j =>
                 {
---
+++ PlaceAgregator/PlaceAgregator.EntityFramework/DesignTimeDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaceAgregator.EntityFramework
+{
+    public class BloggingContextFactory : IDesignTimeDbContextFactory<ApplicationContext>
+    {
+        public ApplicationContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationContext>();
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=PlaceAgregator;Username=postgres;Password=13lu@if3r08");
+
+            return new ApplicationContext(optionsBuilder.Options);
+        }
+    }
+}
