2842511e84f06d2dc51656baf9a700ea41ed69b1
Rivgad <losvermo@gmail.com>
2022-05-21 21:34:01 +0300

    Добавил enum для ролей и extension для получения Description

---
+++ PlaceAgregator/PlaceAgregator.Shared/Models/Enums/Role.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace PlaceAgregator.Shared.Models.Enums
+{
+    public enum Role
+    {
+        [Description("user")]
+        User = 0,
+        
+        [Description("manager")]
+        Manager = 1,
+
+        [Description("admin")]
+        Admin = 2
+    }
+}
