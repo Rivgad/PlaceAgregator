2e6e3889c1c4eae299faa2a08951bbfca4adf290
Rivgad <losvermo@gmail.com>
2022-05-26 18:16:18 +0300

    BookingCreateDTO => BookingRequestCreateDTO

--- PlaceAgregator/PlaceAgregator.Shared/DTOs/Booking/BookingRequestCreateDTO.cs
+++ PlaceAgregator/PlaceAgregator.Shared/DTOs/Booking/BookingRequestCreateDTO.cs
@@ -3,7 +3,7 @@ using System.Text.Json.Serialization;
 
 namespace PlaceAgregator.Shared.DTOs.Booking
 {
-    public class BookingCreateDTO
+    public class BookingRequestCreateDTO
     {
         [Required]
         [JsonPropertyName("placeId")]
