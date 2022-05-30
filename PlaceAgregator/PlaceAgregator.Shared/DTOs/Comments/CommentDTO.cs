6be88c776eff70d6a3ce3a2972bb5ffc20f385e0
Rivgad <losvermo@gmail.com>
2022-05-30 19:04:01 +0300

    RequiredFields in CommentDTO

--- PlaceAgregator/PlaceAgregator.Shared/DTOs/Comments/CommentDTO.cs
+++ PlaceAgregator/PlaceAgregator.Shared/DTOs/Comments/CommentDTO.cs
@@ -5,9 +5,11 @@ namespace PlaceAgregator.Shared.DTOs.Comments
 {
     public class CommentDTO
     {
+        [Required]
         [JsonPropertyName("text")]
         public string Text { get; set; }
 
+        [Required]
         [Range(1, 5)]
         public int Rating { get; set; }
     }
