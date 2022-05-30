f238d43e42e5e4b410b42ecbe5aedce0fddce412
Rivgad <losvermo@gmail.com>
2022-05-30 20:32:26 +0300

    Done Comments API

---
+++ PlaceAgregator/PlaceAgregator.Shared/DTOs/Comments/CommentFilterDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaceAgregator.Shared.DTOs.Comments
+{
+    public class CommentFilterDTO : BaseFilterDTO
+    {
+        public bool? IsBlocked { get; set; }
+        public string? UserName { get; set; }
+        public string? UserId { get; set; }
+        public int? PlaceId { get; set; }
+    }
+}
